40d57a2c1e6c4dc04489824da286c3a95a39a770
Shaon Ahmed <51782102+Shaon99@users.noreply.github.com>
2019-07-31 02:52:00 +0600

    Create Bank.java

---
+++ Bank.java
@@ -0,0 +1,6 @@
+echo "# Bank-Application" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Shaon99/Bank-Application.git
+git push -u origin master
